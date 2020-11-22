d5cbe294d4a89cfa017904c7890625275e4b2407
paul <p.patault@gmail.com>
2020-11-22 18:41:23 +0100

    Update main.ml

--- src/main.ml
+++ src/main.ml
@@ -7,7 +7,7 @@ let () =
   let lexbuf = Lexing.from_channel cin in
   let () = 
     try
-      Parser.program Lexer.token lexbuf 
+      Parser.program Lexer.token lexbuf
       |> check_type_prog
       |> Printf.printf "%b\n"
     with TypeError s -> close_in cin; failwith ("Il y a un probleme de typage : " ^ s) in
