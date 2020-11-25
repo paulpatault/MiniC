259a5c34f815064ef986df486304a10186f06397
paul <p.patault@gmail.com>
2020-11-25 18:28:30 +0100

    error message & tests

--- src/parser_message.ml
+++ src/parser_message.ml
@@ -3,133 +3,133 @@
 (* Please note that the function [message] can raise [Not_found]. *)
 
 let message =
-  fun s ->
-    match s with
-    | 0 ->
-        "<TODO 0>\n"
-    | 4 ->
-        "<TODO 4>\n"
-    | 101 ->
-        "<TODO 101>\n"
-    | 102 ->
-        "<TODO 102>\n"
-    | 26 ->
-        "<TODO 26>\n"
-    | 27 ->
-        "<TODO 27>\n"
-    | 25 ->
-        "<TODO 25>\n"
-    | 65 ->
-        "<TODO 65>\n"
-    | 29 ->
-        "<TODO 29>\n"
-    | 37 ->
-        "<TODO 37>\n"
-    | 40 ->
-        "<TODO 40>\n"
-    | 103 ->
-        "<TODO 103>\n"
-    | 42 ->
-        "<TODO 42>\n"
-    | 43 ->
-        "<TODO 43>\n"
-    | 44 ->
-        "<TODO 44>\n"
-    | 45 ->
-        "<TODO 45>\n"
-    | 46 ->
-        "<TODO 46>\n"
-    | 47 ->
-        "<TODO 47>\n"
-    | 48 ->
-        "<TODO 48>\n"
-    | 49 ->
-        "<TODO 49>\n"
-    | 50 ->
-        "<TODO 50>\n"
-    | 51 ->
-        "<TODO 51>\n"
-    | 30 ->
-        "<TODO 30>\n"
-    | 36 ->
-        "<TODO 36>\n"
-    | 62 ->
-        "<TODO 62>\n"
-    | 52 ->
-        "<TODO 52>\n"
-    | 53 ->
-        "<TODO 53>\n"
-    | 54 ->
-        "<TODO 54>\n"
-    | 55 ->
-        "<TODO 55>\n"
-    | 56 ->
-        "<TODO 56>\n"
-    | 57 ->
-        "<TODO 57>\n"
-    | 58 ->
-        "<TODO 58>\n"
-    | 59 ->
-        "<TODO 59>\n"
-    | 60 ->
-        "<TODO 60>\n"
-    | 61 ->
-        "<TODO 61>\n"
-    | 9 ->
-        "<TODO | 9>\n"
-    | 14 ->
-        "<TODO 14>\n"
-    | 18 ->
-        "<TODO 18>\n"
-    | 19 ->
-        "<TODO 19>\n"
-    | 21 ->
-        "<TODO 21>\n"
-    | 24 ->
-        "<TODO 24>\n"
-    | 67 ->
-        "<TODO 67>\n"
-    | 68 ->
-        "<TODO 68>\n"
-    | 95 ->
-        "<TODO 95>\n"
-    | 96 ->
-        "<TODO 96>\n"
-    | 97 ->
-        "<TODO 97>\n"
-    | 93 ->
-        "<TODO 93>\n"
-    | 22 ->
-        "<TODO 22>\n"
-    | 69 ->
-        "<TODO 69>\n"
-    | 70 ->
-        "<TODO 70>\n"
-    | 11 ->
-        "<TODO 11>\n"
-    | 13 ->
-        "<TODO 13>\n"
-    | 72 ->
-        "<TODO 72>\n"
-    | 73 ->
-        "<TODO 73>\n"
-    | 75 ->
-        "<TODO 75>\n"
-    | 76 ->
-        "<TODO 76>\n"
-    | 77 ->
-        "<TODO 77>\n"
-    | 78 ->
-        "<TODO 78>\n"
-    | 80 ->
-        "<TODO 80>\n"
-    | 81 ->
-        "<TODO 81>\n"
-    | 82 ->
-        "<TODO 82>\n"
-    | 86 ->
-        "<TODO 86>\n"
-    | 88 ->
-        "<TODO 88>\n"
-    | _ ->
-        raise Not_found
+    fun s ->
+        match s with
+        | 0 ->
+            "<Un programme est une suite de variables globales suivient d'une suite de fonctions.>\n"
+        | 4 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 101 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 102 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 26 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 27 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 25 ->
+            "<Un varible ne peut etre definie qu'a partir d'expressions.>\n"
+        | 65 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 29 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 37 ->
+            "<Une operation s'effectue entre deux expressions.>\n"
+        | 40 ->
+            "<Une operation s'effectue entre deux expressions.>\n"
+        | 103 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 42 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 43 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 44 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 45 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 46 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 47 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 48 ->
+            "<Une operation s'effectue entre deux expressions.>\n"
+        | 49 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 50 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 51 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 30 ->
+            "<Les parametres de fonctions sont optionnels, mais s'il y en a, ils doivent etre separes pas des ','.>\n"
+        | 36 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 62 ->
+            "<Les parametres de fonctions sont optionnels, mais s'il y en a, ils doivent etre separes pas des ','.>\n"
+        | 52 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 53 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 54 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 55 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 56 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 57 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 58 ->
+            "<Une comparaison s'effectue entre deux expressions.>\n"
+        | 59 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 60 ->
+            "<Il ne peut y avoir que des operations entre expressions a l'interieur d'une definition de variable.>\n"
+        | 61 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 9 ->
+            "<Un programme est une suite de variables globales suivient d'une suite de fonctions.>\n"
+        | 14 ->
+            "<Les parametres de fonctions sont optionnels, mais s'il y en a, ils doivent etre separes pas des ','.>\n"
+        | 18 ->
+            "<Les parametres de fonctions sont optionnels, mais s'il y en a, ils doivent etre separes pas des ','.>\n"
+        | 19 ->
+            "<Les parametres de fonctions sont optionnels, mais s'il y en a, ils doivent etre separes pas des ','.>\n"
+        | 21 ->
+            "<La syntaxe de definition d'une boucle doit suivre : boucle(condition){ block }. >\n"
+        | 24 ->
+            "<La syntaxe de definition d'une boucle doit suivre : boucle(condition){ block }.>\n"
+        | 67 ->
+            "<La condition d'une boucle doit etre suivie de '{'.>\n"
+        | 68 ->
+            "<Il ne peut y avoir que des instructions a l'interieur d'une boucle.>\n"
+        | 95 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 96 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 97 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 93 ->
+            "<Le corps d'une fontion doit suivre les regles de la grammaire : definitions de variables locales, puis liste d'instructions.>\n"
+        | 22 ->
+            "<Le corps d'une fontion doit suivre les regles de la grammaire : definitions de variables locales, puis liste d'instructions.>\n"
+        | 69 ->
+            "<L'instruction return attend une expression.>\n"
+        | 70 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 11 ->
+            "<Les instructions doivent etre dans une fonction.>\n"
+        | 13 ->
+            "<Les parametre d'une fonction doivent etre entre parentheses.>\n"
+        | 72 ->
+            "<La fonction putchar() attend un parametre de type int.>\n"
+        | 73 ->
+            "<La fonction putchar() attend un parametre de type int.>\n"
+        | 75 ->
+            "<Il manque une '{' apres le la condition du if.>\n"
+        | 76 ->
+            "<Il manque une '{' apres le la condition du if.>\n"
+        | 77 ->
+            "<Une fonction ne peut pas etre definie dans une autre fonction.>\n"
+        | 78 ->
+            "<Les blocs internes des if et else doivent etre encadres par des accolades.>\n"
+        | 80 ->
+            "<Un identifiant ne peut pas etre suivi par un while.>\n"
+        | 81 ->
+            "<Une expression est attendue apres un '='.>\n"
+        | 82 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | 86 ->
+            "<Une fonction doit se terminer par un '}'.>\n"
+        | 88 ->
+            "<Un ';' est attendu a la fin d'une expression.>\n"
+        | _ ->
+            raise Not_found
